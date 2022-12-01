9744f40461edb125b56deff6a811374c01869828
Divisek <113785426+Divisek@users.noreply.github.com>
2022-12-01 11:32:20 +0100

    Add files via upload

---
+++ kalkulacka_s_cykly.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+    Console.WriteLine("Vítejte v kalkulaččce");
+    string pokracovat = "ano";
+    while (pokracovat == "ano") 
+    {
+        Console.WriteLine("Zadejte první číslo:");
+        string vstup_1 = Console.ReadLine();
+        double cislo1 = Convert.ToDouble(vstup_1);
+        Console.WriteLine("Zadejte druhé číslo");
+        string vstup_2 = Console.ReadLine();
+        double cislo2 = Convert.ToDouble(vstup_2);
+        Console.WriteLine("Zvolte si operaci:");
+        Console.WriteLine("1 - sčítání");
+        Console.WriteLine("2 - odečítání");
+        Console.WriteLine("3 - násobení");
+        Console.WriteLine("4 - dělení");
+        int volba = int.Parse(Console.ReadLine());
+        double vysledek = 0;
+
+        switch (volba)
+        {
+            case 1:
+                vysledek = cislo1 + cislo2;
+                break;
+            case 2:
+                vysledek = cislo1 - cislo2;
+                break;
+            case 3:
+                vysledek = cislo1 * cislo2;
+                break;
+            case 4:
+                vysledek = cislo1 / cislo2;
+                break;
+        }
+        if ((volba > 0) && (volba < 5))
+            Console.WriteLine("Výsledek:{0}", vysledek);
+        else
+            Console.WriteLine("Neplatná volba");
+        Console.WriteLine("Chcete počítat něo dalšího?[ano/ne]");
+        pokracovat = Console.ReadLine();
+    }
+    Console.WriteLine("Děkuju za použití kalkulačky, aplikaci ukončíte libovolnopu klávesnicí.");
+    Console.ReadKey();
+}
+
+
+
